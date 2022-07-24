f33827840c578d193e4b90028fee088e3254af49
Si Tran Van <it.sitranvan@gmail.com>
2022-07-24 22:02:03 +0700

    Cấu hình Router và xây dựng cơ chế tải Layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { Fragment } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { publlicRoutes } from '~/routers';
+import { DefaultLayout } from '~/components/Layout';
 function App() {
     return (
-        <div className="App">
-            <h1>Hello</h1>;
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    {publlicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
---
+++ src/routers/index.js
@@ -0,0 +1,19 @@
+import { HeaderOnly } from '~/components/Layout';
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+// Dùng cho những routers không cần đăng nhập vẫn xem được
+const publlicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+// Cần phải đăng nhập, nếu không đăng nhập sẽ chuyển hướng sang trang login
+const privateRoutes = [];
+
+export { publlicRoutes, privateRoutes };
