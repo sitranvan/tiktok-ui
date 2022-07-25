a88bd80c8919f20b71d0057b6826b1749a85e050
Si Tran Van <it.sitranvan@gmail.com>
2022-07-25 14:08:39 +0700

    Dựng khung DefaultLayout

--- src/components/Layout/DefaultLayout/Slidebar/index.js
+++ src/components/Layout/DefaultLayout/Slidebar/index.js
@@ -1,5 +1,13 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
 function Slidebar() {
-    return <h2>Slidebar</h2>;
+    return (
+        <aside className={cx('wrapper')}>
+            <h2>Sidebar</h2>
+        </aside>
+    );
 }
 
 export default Slidebar;
